65433a5620d79a314b489980161c906ad48bfa4f
hasanmehdavi <hasan.mehdavi@talverse.com>
2024-09-28 19:28:04 +0500

    Initial push

---
+++ 21K-4885/Pages/Benifits.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _21K_4885.Pages
+{
+    public class BenifitsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
